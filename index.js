ac93a8bedf773db17defd348b2985d763ca8f03c
ukcasso <ukcasso@gmail.com>
2021-11-04 19:23:30 +0900

    Update index.js

--- index.js
+++ index.js
@@ -56,11 +56,3 @@ app.get('/repos/:username', cache, getRepos);
 app.listen(5000, () => {
   console.log(`App listening on port ${PORT}`)
 })
-// const express = require('express')
-// const app = express()
- 
-// app.get('/', function (req, res) {
-//   res.send('Hello World')
-// })
- 
-// app.listen(3000)
